8263f350f92445946aaee6e2b67dd585213c40e4
ramadak <ramadak@masi-2.ds.vanderbilt.edu>
2019-02-07 14:42:50 -0600

    Table data parsed successfully. Need to work on importing to REDCap

--- push_to_redcap.py
+++ push_to_redcap.py
@@ -38,7 +38,7 @@ def open_file(filepath):
     """
     try:
         xl_file = open(filepath,"r")
-        LOGGER.info("Opening xls file as txt")
+        LOGGER.info("Opening "+filepath)
         return xl_file
     except:
         raise SystemExit("ERROR: Check path and permissions of file you are trying to upload")
@@ -56,18 +56,19 @@ def list_data_by_line(data):
             table_data.append(line)
     return table_data,stagnant_data
 
-def table_to_dict(table_data):
+def table_to_dict(table_data,OPTIONS):
     """
 
     """
     roi_list = []
-    table_values = get_values_and_variables(table_data)
+    table_values = get_values_and_variables(table_data,OPTIONS)
     return table_values
 
-def get_values_and_variables(table_data):
+def get_values_and_variables(table_data,OPTIONS):
     """
     """
     table_values = []
+    settings_dict = read_settings_file(OPTIONS)
     for line in table_data:
         if "ROI" in line or "VOI" in line:
             headers = line
@@ -76,16 +77,29 @@ def get_values_and_variables(table_data):
             line = re.split(r'\t+',line)
             dictionary = dict(zip(headers,line))
             for key in dictionary.keys():
-                table_values.append(dictionary.get('ROI_Name')+"_"+key+ \
-                                    " : "+dictionary.get(key))
+                if key in settings_dict.keys():
+                    table_values.append(dictionary.get('ROI_Name')+"_"+ \
+                               settings_dict.get(key)+" : "+dictionary.get(key))
     return table_values
 
-#def split_by_tab():
-
-
-def replace_variable_name():
+def update_redcap(table_values,settings_dict,project):
     """
     """
+#    for pair in table_values:
+
+
+def read_settings_file(OPTIONS):
+    """
+    """
+    settings = open_file(OPTIONS.settings)
+    settings_file = settings.read()
+    data_var,redcap_var = [],[]
+    for line in settings_file.splitlines():
+        if line!='':
+            data_var.append(re.split(r'=+',line)[0])
+            redcap_var.append(re.split(r'=+',line)[1])
+    var_dict = dict(zip(data_var,redcap_var))
+    return var_dict
 
 
 def add_to_parser():
@@ -100,6 +114,8 @@ def add_to_parser():
                         help = 'path of file that needs to be uploaded')
     parser.add_argument("-s","--study",dest='form',default=None,\
                         help='Name of study on redcap. Replace spaces with _')
+    parser.add_argument("-i","--settings",dest='settings',default=None,\
+                        help='path to settings file of the study')
     return parser
 
 def execute():
@@ -117,7 +133,8 @@ def execute():
     forms = get_form_names(project)
     doc = open_file(OPTIONS.path)
     table_data,stagnant_data = list_data_by_line(doc)
-    formatted_table_data = table_to_dict(table_data)
+    formatted_table_data = table_to_dict(table_data,OPTIONS)
+    upload_to_redcap(project,stagnant_data,formatted_table_data)
 
 if __name__ == '__main__':
     execute()
