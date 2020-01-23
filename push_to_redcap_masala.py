dd978154cd708b0b4e17725e281c3b0806141781
karthikmasi <karthik.ramadass@vanderbilt.edu>
2020-01-23 16:03:24 -0600

    masala study uploader

--- push_to_redcap_masala.py
+++ push_to_redcap_masala.py
@@ -9,6 +9,22 @@ import argpars
 import logging as LOGGER
 import re
 
+
+def redcap_project_access(API_KEY):
+    """
+    Access point to REDCap form
+    :param API_KEY:string with REDCap database API_KEY
+    :return: redcap Project Object
+    """
+    try:
+        project = redcap.Project('https://redcap.vanderbilt.edu/api/', API_KEY)
+    except:
+        LOGGER.error('ERROR: Could not access redcap. Either wrong API_URL/API_KEY or redcap down.')
+        sys.exit(1)
+    return project
+
+
+
 def execute():
     """
     point of execution
