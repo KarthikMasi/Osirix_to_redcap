37712c4367fd4f023372d4b5fb22f28b7625889a
karthikmasi <karthik.ramadass@vanderbilt.edu>
2022-05-11 15:10:05 -0500

    Initial architecture

---
+++ push_to_redcap.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import redcap
+import os
+import sys 
+import argparse
+import logging as LOGGER
+
+
+def open_file(filepath):
+
+def redcap_project_access(API_KEY):
+
+def link_csv_to_redcap_variables(table_data):
+
+def push_to_redcap(redcap_dict):
+
+def execute():
+
+if __name__ == '__main__':
+    execute()
