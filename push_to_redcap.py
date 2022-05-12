fdf54627ff13c3d4671367a3406012085dd0e660
karthikmasi <karthik.ramadass@vanderbilt.edu>
2022-05-12 18:22:58 -0500

    Ready for testing

--- push_to_redcap.py
+++ push_to_redcap.py
@@ -13,11 +13,6 @@ redcap_variables=['record_id','username','current_date','id','studyid',\
  'sat_area_surf','muscle_mean','imat_mean','vat_mean','sat_mean']
 
 def open_file(filepath):
-	"""
-    Opens file and returns the file object ready to be read.
-    :param filepath:string of path to file
-    :return: file object
-    """
     try:
         xl_file = open(filepath,"r")
         return xl_file
@@ -26,10 +21,8 @@ def open_file(filepath):
         sys.exit(1)
 
 def redcap_project_access(API_KEY):
-	"""
-    Access point to REDCap form
-    :param API_KEY:string with REDCap database API_KEY
-    :return: redcap Project Object
+    """
+    Returns the redcap project object
     """
     try:
         project = redcap.Project('https://redcap.vanderbilt.edu/api/', API_KEY)
@@ -55,27 +48,38 @@ def link_csv_to_redcap_variables(csv_path,png_path,project):
           redcap_variables[5]:records[4],redcap_variables[6]:records[5],\
           redcap_variables[7]:records[6],redcap_variables[8]:records[7],\
           redcap_variables[9]:records[8],redcap_variables[10]:records[9],\
-          redcap_variables[11]:records[10],redcap_variables[12]:records[11],\
+    redcap_variables[11]:records[10],redcap_variables[12]:records[11],\
           redcap_variables[13]:records[12]
                 }
         redcap_update_list.append(redcap_dict)
     return redcap_upload_list
 
-def push_to_redcap(redcap_list,project):
+def push_to_redcap(redcap_list,csv_file,png_file,project,OPTIONS,record_id):
+    """
+    """
+    try:
+        log_var = project.import_records(redcap_list, overwrite='normal' \
+                                         , return_format='json', date_format='MDY')
+        LOGGER.info("Records Upload COMPLETE! " + OPTIONS.path)
+        response_csv = project.import_file(record_id,'csv_file',csv_file,open(csv_file,"r"))
+        response_png = project.import_file(record_id,'png_file',png_file,open(png_file,"r"))
+        LOGGER.info("Files uploaded to record: " + record_id)
+    except (redcap.RedcapError, ValueError) as redcaperror:
+        LOGGER.error(str(redcaperror))
+        LOGGER.error(OPTIONS.path)
+        sys.exit(1)
 
 def add_to_parser():
-	"""
+    """
 	Method to add arguments to default parser
 	:return: parser object
 	"""
-	parser = argparse.ArgumentParser(description='Crontabs manager')
-    parser.add_argument("-k","--key",dest='API_KEY',default=None, required=True,\
-                        help='API key to REDCap Database')
-    parser.add_argument("-p","--path",dest='path',default=None, required=True,\
-                        help = 'path of directory containing files that need to be uploaded')
-    parser.add_argument("-l","--logfile",dest='log',default=None, required=True,\
-                        help='Log file path and name')
-	return parser
+    parser = argparse.ArgumentParser(description='Crontabs manager')
+
+    parser.add_argument("-k","--key",dest='API_KEY',default=None, required=True,help='API key to REDCap Database')
+    parser.add_argument("-p","--path",dest='path',default=None, required=True,help = 'path of directory containing files that need to be uploaded')
+    parser.add_argument("-l","--logfile",dest='log',default=None, required=True,help='Log file path and name')
+    return parser
 
 def execute():
     parser = add_to_parser()
@@ -88,6 +92,8 @@ def execute():
     project = redcap_project_access(OPTIONS.API_KEY)
     csv_file = glob.glob(OPTIONS.path+"/*.csv")[0]
     png_file = glob.glob(OPTIONS.path+"/*.png")[0]
+    upload_data = link_csv_to_redcap_variables(csv_file,png_file,project)
+    push_to_redcap(upload_data,csv_file,png_file,project,OPTIONS,png_file.spit('/')[-1].split('.')[0])
 
 if __name__ == '__main__':
     execute()
