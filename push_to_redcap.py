0ba9ecac23ef738475d3c47691e75c06fd8d02f3
karthikmasi <karthik.ramadass@vanderbilt.edu>
2022-05-12 18:55:15 -0500

    Few more bugs fixed

--- push_to_redcap.py
+++ push_to_redcap.py
@@ -25,10 +25,10 @@ def redcap_project_access(API_KEY):
     Returns the redcap project object
     """
     try:
-        project = redcap.Project('https://redcap.vanderbilt.edu/api/', API_KEY)
+        project = redcap.Project('https://redcap.vanderbilt.edu/api/',API_KEY)
     except:
         LOGGER.error('ERROR: Could not access redcap. Either wrong API_URL/API_KEY or redcap down.')
-
+    return project
 def link_csv_to_redcap_variables(csv_path,png_path,project):
     """
     """
@@ -40,16 +40,16 @@ def link_csv_to_redcap_variables(csv_path,png_path,project):
     all_lines = csv_file.read().splitlines()
     lines_for_upload = all_lines[3:] #Removing the headers
     record_id = png_path.split('/')[-1].split('.')[0]
-    for line in all_lines:
+    for line in lines_for_upload:
         records = line.split(',')
-        redcap_dict = {redcap_variables[0]:record_id,\
+        redcap_dict = {unicode(redcap_variables[0], "utf-8"):record_id,\
           redcap_variables[1]:records[0],redcap_variables[2]:records[1],\
           redcap_variables[3]:records[2],redcap_variables[4]:records[3],\
           redcap_variables[5]:records[4],redcap_variables[6]:records[5],\
           redcap_variables[7]:records[6],redcap_variables[8]:records[7],\
           redcap_variables[9]:records[8],redcap_variables[10]:records[9],\
     redcap_variables[11]:records[10],redcap_variables[12]:records[11],\
-          redcap_variables[13]:records[12]
+          redcap_variables[13]:records[12],'abdominal_area_analysis_complete':2
                 }
         redcap_upload_list.append(redcap_dict)
     return redcap_upload_list
@@ -58,8 +58,8 @@ def push_to_redcap(redcap_list,csv_file,png_file,project,OPTIONS,record_id):
     """
     """
     try:
-        log_var = project.import_records(redcap_list, overwrite='normal' \
-                                         , return_format='json', date_format='MDY')
+        log_var = project.import_records(redcap_list, overwrite='normal',\
+                                return_format='json', date_format='MDY')
         LOGGER.info("Records Upload COMPLETE! " + OPTIONS.path)
         response_csv = project.import_file(record_id,'csv_file',csv_file,open(csv_file,"r"))
         response_png = project.import_file(record_id,'png_file',png_file,open(png_file,"r"))
@@ -93,7 +93,7 @@ def execute():
     csv_file = glob.glob(OPTIONS.path+"/*.csv")[0]
     png_file = glob.glob(OPTIONS.path+"/*.png")[0]
     upload_data = link_csv_to_redcap_variables(csv_file,png_file,project)
-    push_to_redcap(upload_data,csv_file,png_file,project,OPTIONS,png_file.spit('/')[-1].split('.')[0])
+    push_to_redcap(upload_data,csv_file,png_file,project,OPTIONS,png_file.split('/')[-1].split('.')[0])
 
 if __name__ == '__main__':
     execute()
